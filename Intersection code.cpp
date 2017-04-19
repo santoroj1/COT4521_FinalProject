2a9dccdab43271c6c0038bc4a18445cc52381488
santoroj1 <santoroj@mail.usf.edu>
2017-04-18 23:32:41 -0400

    Delete Intersection code.cpp

--- Intersection code.cpp
+++
@@ -1,5 +0,0 @@
-//Idea to detect interesection 
-//Use vector do detect extreme points, 
-//		if the extreme points closest to each other are past each other, then they intersect
-//		if the Extreme point of relative upper polygon is below the extreme of the relatve
-//			lower polygon, then they intersect
